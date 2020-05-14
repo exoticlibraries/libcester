b76fe6745775290a04b717f90c03ac55341ddff1
Adewale Azeez <azeezadewale98@gmail.com>
2020-05-14 18:17:53 +0100

    create the deploy.sh file to push to github

---
+++ docs/deploy.sh
@@ -0,0 +1,12 @@
+
+mkdir -p dist/gh-pages/
+cd dist/gh-pages/
+git clone -b gh-pages https://github.com/exoticlibraries/libcester.git
+cd libcester/
+cp -r ../../../docs/build/html/* ./
+ls
+# git config --local user.email "azeezadewale98@gmail.com"
+# git config --local user.name "travis-ci.org"
+# git add .; git commit -m "Travis build=${TRAVIS_BUILD_NUMBER}. Update Documentation from Travis CI"
+# git push -f https://Thecarisma:${GITHUB_TOKEN}@github.com/exoticlibraries/libcester.git HEAD:gh-pages;
+cd ../../../

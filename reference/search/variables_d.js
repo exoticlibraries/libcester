1d6035d42d336ddf3e6fe40da041b2fd49402845
travis-ci.org <azeezadewale98@gmail.com>
2020-07-02 17:16:04 +0000

    Travis build=44. Update Documentation from Travis CI

--- reference/search/variables_d.js
+++ reference/search/variables_d.js
@@ -3,7 +3,6 @@ var searchData=
   ['selected_5ftest_5fcases_5ffound',['selected_test_cases_found',['../structsuper__test__instance.html#aa29ec99f02330228855474a8d93edb33',1,'super_test_instance']]],
   ['selected_5ftest_5fcases_5fnames',['selected_test_cases_names',['../structsuper__test__instance.html#acefe9ae1d777e134e6ea99a5cc9ced7f',1,'super_test_instance']]],
   ['selected_5ftest_5fcases_5fsize',['selected_test_cases_size',['../structsuper__test__instance.html#ad4f3871744486147563bfa5dcf108be9',1,'super_test_instance']]],
-  ['signal',['signal',['../cester_8h.html#a6fc82cd60ac4b31003cf10983a4d5cde',1,'cester.h']]],
   ['single_5foutput_5fonly',['single_output_only',['../structsuper__test__instance.html#a20c6f80a327f4898508e0ef68900d490',1,'super_test_instance']]],
   ['size',['size',['../structcester__array__struct.html#ae754b1474dc0daf8fd50194dd26398ee',1,'cester_array_struct']]],
   ['skipped_5ftest_5fcount',['skipped_test_count',['../structsuper__test__instance.html#a07f27c3cd0b7054c700a148013876922',1,'super_test_instance']]],

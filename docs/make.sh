e4e168f3507673e1e8832ba78dc484445790ba02
Adewale Azeez <azeezadewale98@gmail.com>
2020-10-15 02:06:19 +0100

    remove travis CI/CD and use Github Action for gh-page deployment and cmake regression

--- docs/make.sh
+++ docs/make.sh
@@ -1,6 +1,6 @@
 
 rm -R build/
-sphinx-build -b html -d build/doctrees   ./ build/html
+python -m sphinx  -b html -d build/doctrees   ./ build/html
 doxygen
 cp ../installscripts/cester.ps1 ./build/html/
 cp ../installscripts/cester.sh ./build/html/

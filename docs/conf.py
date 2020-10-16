3333582fc5646a2529349ad53aedc814897f3b64
Adewale Azeez <azeezadewale98@gmail.com>
2020-10-16 02:20:23 +0100

    add master_doc=index for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,7 @@ author = 'Adewale Azeez and Other Contributors'
 
 html_theme_path = [themata.get_html_theme_path()]
 html_theme = 'milkish'
+master_doc = 'index'
 html_favicon = 'libcester.png'
 main_doc = 'index'
 html_static_path = ['_static']
